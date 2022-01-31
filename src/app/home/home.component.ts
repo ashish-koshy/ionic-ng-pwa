7f6a939b66da2d64a3cd1b5094197cb362fa6a28
Ashish Koshy <ashish.avior@gmail.com>
2022-01-31 15:43:44 +0530

    addded .env config

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class HomeComponent {
+  public environment = environment;
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  /** 
+   * `ENVIRONMENT_NAME` is a placeholder token that gets 
+   * replaced by a value specific to your environment (DEV, QA, PROD etc).
+   * Ideally, this should be done by your build pipline.
+   * */
+  name: '#{ENVIRONMENT_NAME}#'
 };
